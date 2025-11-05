aeecc37c39d787cfe4d51e2b742600d7e57b682b
maxjlee08 <maxjlee08@gmail.com>
2025-11-05 16:05:27 +0000

    first commit

--- MyLLTester.java
+++ MyLLTester.java
@@ -1,8 +1,12 @@
-public class MyLLTester.java
+public class MyLLTester
 {
   public static void main(String [] args)
   {
-    
+  MyLinkedList list = new MyLinkedList();
+
+  System.out.println("Is list empty?" + list.isEmpty());
+
+
 
   } // end main
   
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,46 @@
 public class MyLinkedList
 {
+    private ListNode head;
+    public int size;
 
+    public MyLinkedList(){
+        head = null;
+        size = 0;
+        
+    }
+
+    //# of elements
+    public int size(){
+        return size;
+    }
+    
+    //true if list is empty
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    //adds new node to the end of the list
+    public boolean add(Object newItem){
+        return (newItem);
+    }
+
+    //Adds a node to the beginning
+    public boolean addFirst(Object newItem){
+        ListNode temp = new ListNode(newItem);
+        temp.setNext(head);
+        head = temp;
+        size++;
+        return true;
+        
+
+        
+        }
+    }
+
+    
+
+    
+
+    
 
 }
